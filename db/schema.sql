015c3deaa94b7431aba0005854c99ce38ebf91b0
blingdahl <ben.lindahl@gmail.com>
2025-05-20 12:28:18 -0700

    Add the sql to create the db (dropping first if needed)

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,16 @@
+-- To run this script, use:
+--   mysql -u root -p < db/schema.sql
+
+DROP DATABASE IF EXISTS tictactoe;
+DROP USER IF EXISTS 'tictactoe_user'@'localhost';
+
+CREATE DATABASE tictactoe;
+CREATE USER 'tictactoe_user'@'localhost' IDENTIFIED BY 'yourpassword';
+GRANT ALL PRIVILEGES ON tictactoe.* TO 'tictactoe_user'@'localhost';
+FLUSH PRIVILEGES;
+
+USE tictactoe;
+
 CREATE TABLE IF NOT EXISTS Games (
   id INT AUTO_INCREMENT PRIMARY KEY,
   player1 VARCHAR(64) NOT NULL,
